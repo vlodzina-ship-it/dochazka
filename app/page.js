b415b18df9095f732d7ec58336b2b11da010c4d3
vlodzina-ship-it <vlodzina@gmail.com>
2026-03-10 09:54:19 +0100

    Delete app/page.js

--- app/page.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{padding:40,fontFamily:"Arial"}}>
-      <h1>Docházkový systém</h1>
-      <p>Aplikace běží správně.</p>
-    </div>
-  );
-}
