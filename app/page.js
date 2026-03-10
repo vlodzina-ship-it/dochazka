525b5a61882241e9409881ef0726d78a7727c350
vlodzina-ship-it <vlodzina@gmail.com>
2026-03-10 09:47:16 +0100

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{padding:40,fontFamily:"Arial"}}>
+      <h1>Docházkový systém</h1>
+      <p>Aplikace běží správně.</p>
+    </div>
+  );
+}
